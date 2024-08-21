c6f7e45ac691795e9df3c57a2b52139ca64e96f6
gary <gary@byoteki.com>
2024-08-20 23:47:23 -0700

    add origins

--- src/RandomClass.tsx
+++ src/RandomClass.tsx
@@ -1,22 +1,69 @@
 type ClassType = {
   name: string;
+  possibleOrigins?: string[];
 };
 
 const seeker: ClassType = {
   name: "Seeker of the Left Hand Path",
+  possibleOrigins: [
+    `Obsessed with reading, you alienated yourself from society in the pursuit of esoteric knowledge. Now society alienates you for your inability to communicate rationally.`,
+    `Your parents kept you indoors during inclement weather but after your ferocious tantrums, the sky always seemed to clear. Villagers avoided you and whispered as you walked by.`,
+    `While staring into a telescope you saw the face of the Megatherion and came to understand the falsehood of the cosmos.`,
+    `Having labored as a low level scribe your entire life, you managed to avoid ever getting any better at your craft. If anything, you are worse now than when your mind was young and supple. It is not hope that drives you in your quest for knowledge, but a banal fear of inadequacy and tedium.`,
+    `Your pursuits have caused changes in your personality. You have become quick to anger and prone to long-winded psychobabble rationalizing your lack of compassion as a warranted response to naive uneducated people mindlessly subjugating will to so-called “reality.” Your family has cast you out and will not miss you.`,
+    `You have somehow wiped your memory clean of the past. Consciousness is a disease and memory is a symptom. You never remember anything that happened yesterday.`,
+  ],
 };
 
 const mountaineer: ClassType = {
   name: "Mountaineer",
+  possibleOrigins: [
+    `You were abandoned in the wilderness as a child and raised yourself. It was not until your adolescence that you first met people.`,
+    `Coming from wealth, you rejected the path set before you and instead chose to follow your luxurious hobby of mountain expeditions. While you maintained connection with your family and status enough to maintain a financial safety net, a disaster brought them and all their property to ruin.`,
+    `You never felt at home in a house. Sleeping under the stars abates the claustrophobia of walls and the threatening anxious sounds of society. You have been content until now to live as a hermit in one of the nearby caves.`,
+    `On the last expedition, your party lost their way. The harsh winter convinced you that not all would survive. Not all of you did. But their bodies weren’t wasted and you did not go hungry.`,
+  ],
 };
 
-const classTypes: ClassType[] = [seeker, mountaineer];
+const offspring: ClassType = {
+  name: "Offspring of Pan",
+  possibleOrigins: [
+    `Mocked by the human children, you left home early with hate in your heart for the lesser species.`,
+    `Having made every attempt to hide the resemblance to your father, you will cower in shame no longer.`,
+    `After a joyous life in a small village, you heard the call of the Annihilation Song, set your home aflame, and left the village burning to begin your pilgrimage.`,
+    `The beast within you must be given its time. Fearful of the animalistic rage that whelms inside you, you have abandoned your many lovers and even more offspring.`,
+    `Having never known your father, your animal features only appeared as you entered adulthood. Your mother, sharing your birth story, has stirred in you a quest for patricide.`,
+    `A lonely child, you felt most at home with the livestock. Great was the JOY you felt when setting them free. You are compelled to free all livestock whether by destroying their worldly cages or releasing them from their corporeal prison`,
+  ],
+};
+
+const alchemist: ClassType = {
+  name: "Alchemist",
+  possibleOrigins: [
+    `Having spent your best years apprenticing with a master moron, you learned little to nothing except from stolen books and your own blasphemous experiments.`,
+    `Those of culture and those below it found your eccentricities equally as repulsive and blasphemous; arrested on charges of consorting with devils, you escaped by poisoning the water supply of the whole simple-minded city, leaving you unattended for the weeks you needed to tunnel your way out of your cell.`,
+    `With only vague recollections of the Lunarians who descended through the cosmos to leave you at your adopted parents’ doorstep, you know that the Rebis is the key to returning to your rightful, pale silver home.`,
+    `After a long bout of plague, you awoke in a dark, sealed coffin, clawed your way out, and returned home with a newly sparked interest in the occult sciences. After six more deaths, you can’t help but wonder: will your next be your last?  `,
+  ],
+};
+
+const classTypes: ClassType[] = [seeker, mountaineer, alchemist, offspring];
 
 export const RandomClass = () => {
   const c = classTypes[Math.floor(Math.random() * classTypes.length)];
   return (
     <p>
       <b>Class:</b> {c.name}
+      {c.possibleOrigins && (
+        <p className="mt-10">
+          <b>Origin:</b>{" "}
+          {
+            c.possibleOrigins[
+              Math.floor(Math.random() * c.possibleOrigins.length)
+            ]
+          }
+        </p>
+      )}
     </p>
   );
 };
