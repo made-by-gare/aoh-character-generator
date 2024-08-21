5c1561a0dea9abce9e23cfaed0db6fc2e6ec9756
gary <gary@byoteki.com>
2024-08-20 22:42:48 -0700

    Add button to rerender and random name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { RandomName } from "./RandomName";
 
 function App() {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
   return (
     <div className="App m-10">
       <header className="App-header">
         <h1>Abyss of Hallucinations</h1>
         <h2>Character Generator</h2>
+        <button onClick={forceUpdate}>Roll New Character</button>
+        <p>
+          <b>Name:</b> <RandomName />
+        </p>
       </header>
     </div>
   );
